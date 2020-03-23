c84d4658646562a483e962285e74a003b656ca67
Julien Pause <julien.pause@epitech.eu>
2020-03-23 21:56:03 +0400

    Add orientation_enum.h

---
+++ include/enumerations/orientation_enum.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_tetris_2019
+** File description:
+** orientation_enum
+*/
+
+#ifndef ORIENTATION_ENUM_H_
+    #define ORIENTATION_ENUM_H_
+
+    typedef enum orientation {
+        LEFT,
+        RIGHT
+    } orientation_t;
+#endif
