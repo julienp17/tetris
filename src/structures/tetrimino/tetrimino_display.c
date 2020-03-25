9de665cb84a2520987671f118863b0033ca40608
Julien Pause <julien.pause@epitech.eu>
2020-03-25 14:45:34 +0400

    Rename tetrimino_print to tetrimino_display

--- src/structures/tetrimino/tetrimino_display.c
+++ src/structures/tetrimino/tetrimino_display.c
@@ -2,18 +2,19 @@
 ** EPITECH PROJECT, 2020
 ** PSU_tetris_2019
 ** File description:
-** tetrimino_print
+** Display a tetrimino
 */
 
 #include <ncurses.h>
 #include "tetrimino.h"
 
-void tetrimino_print(tetrimino_t *tetrimino)
+void tetrimino_display(tetrimino_t *tetrimino, int y, int x)
 {
     uchar square_size = 0;
 
     square_size = MAX(tetrimino->height, tetrimino->width);
     for (uint row = 0 ; row < square_size ; row++) {
+        move(y + row, x);
         for (uint col = 0 ; col < square_size ; col++) {
             if (tetrimino->shape[row][col] == tetrimino->color) {
                 attron(COLOR_PAIR(tetrimino->color));
@@ -23,7 +24,6 @@ void tetrimino_print(tetrimino_t *tetrimino)
                 addch(' ');
             }
         }
-        addch('\n');
     }
-    refresh();
+    move(y, x);
 }
