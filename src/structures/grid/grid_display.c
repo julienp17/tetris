cf5ad6798f3d9acd96bc243afea26d3b5714cc97
Julien Pause <julien.pause@epitech.eu>
2020-03-25 14:45:52 +0400

    Rename grid_print to grid_display

--- src/structures/grid/grid_display.c
+++ src/structures/grid/grid_display.c
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2020
 ** PSU_tetris_2019
 ** File description:
-** Print a grid in ncurses
+** Displays a grid in ncurses
 */
 
 #include <ncurses.h>
@@ -11,10 +11,12 @@
 
 static void grid_print_border(uint width);
 
-void grid_print(grid_t *grid)
+void grid_display(grid_t *grid, int y, int x)
 {
+    move(y, x);
     grid_print_border(grid->width);
     for (uint row = 0 ; row < grid->height ; row++) {
+        move(y + row + 1, x);
         addch('|');
         for (uint col = 0 ; col < grid->width ; col++) {
             attron(COLOR_PAIR(grid->cells[row][col]));
@@ -22,10 +24,10 @@ void grid_print(grid_t *grid)
             attroff(COLOR_PAIR(grid->cells[row][col]));
         }
         addch('|');
-        addch('\n');
     }
+    move(y + grid->height + 1, x);
     grid_print_border(grid->width);
-    refresh();
+    move(y, x);
 }
 
 static void grid_print_border(uint width)
@@ -34,5 +36,4 @@ static void grid_print_border(uint width)
     for (uint col = 0 ; col < width ; col++)
         addch('-');
     addch('+');
-    addch('\n');
 }
