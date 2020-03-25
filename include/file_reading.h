e48757cf57954714b4b518d92d858f80c5009efe
Julien Pause <julien.pause@epitech.eu>
2020-03-25 11:55:55 +0400

    Add get_nb_files_in_dir function prototype in file_reading.h

--- include/file_reading.h
+++ include/file_reading.h
@@ -8,6 +8,7 @@
 #ifndef FILE_READING_H_
     #define FILE_READING_H_
 
+    int get_nb_files_in_dir(char const *pathname);
     unsigned int get_file_size(char const *filepath);
     char *get_file_buffer(char const *filepath);
     char **get_file_lines(char const *pathname);
