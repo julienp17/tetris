c2032e3df049272f828d82eae83b9178a210b158
Julien Pause <julien.pause@epitech.eu>
2020-03-27 15:52:19 +0400

    Refactor display_game_info.c

--- src/display_game_info.c
+++ src/display_game_info.c
@@ -8,14 +8,10 @@
 #include <ncurses.h>
 #include <time.h>
 #include "game_info.h"
-#include "tetris.h"
+#include "display.h"
 
 void display_game_info(game_info_t *game_info, clock_t game_clock, int y, int x)
 {
-    int previous_y = 0;
-    int previous_x = 0;
-
-    getyx(stdscr, previous_y, previous_x);
     mvprintw(y, x, "Game info");
     display_box(y + 1, x, 10, 20);
     mvprintw(y + 2, x + 1, "High Score : %d", game_info->high_score);
@@ -23,5 +19,5 @@ void display_game_info(game_info_t *game_info, clock_t game_clock, int y, int x)
     mvprintw(y + 6, x + 1, "Level : %d", game_info->level);
     mvprintw(y + 8, x + 1, "Lines removed : %d", game_info->lines_removed);
     mvprintw(y + 10, x + 1, "Timer : %d", game_clock);
-    move(previous_y, previous_x);
+    refresh();
 }
