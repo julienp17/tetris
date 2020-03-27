02c1eb1479166147c45808589d40161811405127
Julien Pause <julien.pause@epitech.eu>
2020-03-27 14:56:16 +0400

    Remove width and height of tetrimino and replace by vector size and pos

--- include/tetrimino.h
+++ include/tetrimino.h
@@ -9,6 +9,7 @@
     #define TETRIMINO_H_
 
     #include <stdbool.h>
+    #include "vector.h"
     #include "grid.h"
     #include "orientation_enum.h"
 
@@ -20,9 +21,10 @@
     typedef unsigned char **shape_t;
     typedef struct tetrimino {
         char *name;
-        uchar height;
-        uchar width;
         uchar color;
+        vec_t pos;
+        vec_t size;
+        uint square_size;
         shape_t shape;
     } tetrimino_t;
 
@@ -32,11 +34,11 @@
     void tetrimino_destroy(tetrimino_t *tetrimino);
     bool tetrimino_can_fall(tetrimino_t *tetrimino, grid_t *grid);
 
-    shape_t tetrimino_shape_create(uint width, uint height);
+    shape_t tetrimino_shape_create(vec_t size);
     shape_t tetrimino_shape_dup(tetrimino_t const *tetrimino);
 
     void tetrimino_rotate(tetrimino_t *tetrimino, orientation_t orientation);
-    void tetrimino_display(tetrimino_t *tetrimino, int y, int x);
+    void tetrimino_display(tetrimino_t *tetrimino);
 
     tetrimino_t **get_tetriminos_from_dir(char const *dir_path);
 #endif
