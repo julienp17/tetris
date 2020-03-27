79f836067283a303072a6f36b548dc5b7857f8fa
Julien Pause <julien.pause@epitech.eu>
2020-03-27 15:50:41 +0400

    Add tetrimino_can_fall function prototype in tetris.h

--- include/tetris.h
+++ include/tetris.h
@@ -28,11 +28,7 @@
 
     int tetris(void);
     int ncurses_init(void);
+    bool tetrimino_can_fall(tetrimino_t *tetrimino, grid_t *grid);
 
     bool print_help(int ac, char **av);
-    void clear_zone(int y_beg, int x_beg, int y_end, int x_end);
-    void display_box(int y, int x, int height, int width);
-    void display_next_tetrimino(tetrimino_t *tetrimino, int y, int x);
-    void display_game_info(game_info_t *game_info, clock_t game_clock,
-                            int y, int x);
 #endif
